c75a213315aa48dd1199a923bb80173388309955
Alexis <alexismedrano9139@gmail.com>
2021-09-20 20:36:56 -0300

    Se agrega funcionalidades de fechas

---
+++ ScreenPlay2/Acciones/AdjuntarArchivo.cs
@@ -0,0 +1,26 @@
+﻿using AventStack.ExtentReports;
+using OpenQA.Selenium;
+using ScreenPlay2.Bases;
+using ScreenPlay2.Reportes;
+using System;
+
+namespace ScreenPlay2.Acciones
+{
+    public static class AdjuntarArchivo
+    {
+        public static void Adjuntar(By locator, Reporte report, string adjunto)
+        {
+            try
+            {
+                var element = Driver.driver.FindElement(locator);
+                element.SendKeys(adjunto);
+                report.addLogScreenCapture(Status.Pass, $"Fue posible ingresar el archivo '{adjunto}'");
+            }
+            catch (Exception ex)
+            {
+                report.addLogScreenCapture(Status.Fail, $"No fue posible ingresar el texto '{adjunto}'");
+                throw ex;
+            }
+        }
+    }
+}
---
+++ ScreenPlay2/Acciones/Fecha.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenPlay2.Acciones
+{
+    public static class Fecha
+    {
+        public static string FechaAgregarDias(int dias)
+        {
+            string fecha = DateTime.Now.AddDays(dias).ToString("dd/MM/yyyy");
+            return fecha;
+        }
+
+        public static string ObtenerDia()
+        {
+            var fecha = DateTime.Now.Day;
+            return fecha.ToString();
+        }
+
+        public static string ObtenerMes()
+        {
+            var fecha = DateTime.Now.Month;
+            return fecha.ToString();
+        }
+
+        public static string ObtenerAño()
+        {
+            var fecha = DateTime.Now.Year;
+            return fecha.ToString();
+        }
+    }
+}
